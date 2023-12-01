6a6e77d90d7b02bb34794d35cf2e5f45fe28cf75
MedHachami <moha.hach.9@gmail.com>
2023-12-01 15:07:22 +0100

    Update auth.php

--- app/views/pages/auth.php
+++ app/views/pages/auth.php
@@ -8,6 +8,13 @@
       href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
       rel="stylesheet"
     />
+    <!-- Tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+     <link
+      href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/tailwind.css">
    
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
   </head>
