18a0359b8303595f980b9bcad1b2cebb685541cd
BelhajMokhlis <belhajmokhlis@gmail.com>
2023-12-02 21:01:57 +0100

    footer V1

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,11 @@
+<footer class="bg-gray-200">
+        <div class="container mx-auto px-6 py-3 flex justify-between items-center">
+            <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">Kamikaze_Blog</a>
+            <p class="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+        </div>
+    </footer>
   <script src="<?php echo URLROOT ; ?>/js/main.js"></script>
 </body>
 </html>
+
+
