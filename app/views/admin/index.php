cd60da04c799352bcc7a1761c18d01b7519f926b
MedHachami <moha.hach.9@gmail.com>
2023-12-02 16:46:36 +0100

    Update index.php

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -3,91 +3,186 @@
   <div class="flex flex-col flex-1 min-h-screen overflow-x-hidden overflow-y-auto">
     
   <?php require APPROOT . '/views/inc/adminNavBar.php'; ?>
-          <!-- Main content -->
-          <div class="flex-1 items-center justify-center flex-1 h-full p-4">
-             <!-- Main content -->
-          <main>
-            <!-- Content header -->
-            <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
-              <h1 class="text-2xl font-semibold">Dashboard</h1>
-              
-            </div>
-            <!-- End Content header -->
+    <!-- Main content -->
+    <div class="flex-1 items-center justify-center flex-1 h-full p-4">
+       <!-- Main content -->
+      <main>
+        <!-- Content header -->
+        <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+          <h1 class="text-2xl font-semibold">Dashboard</h1>
+          
+        </div>
+        <!-- End Content header -->
 
-            <!-- Content -->
-            <div class="mt-2">
-              <!-- State cards -->
-              <div class="grid grid-cols-1 gap-8 p-4 lg:grid-cols-2 xl:grid-cols-4">
-                <!-- Value card -->
-                <div class="flex items-center justify-between p-4 bg-white rounded-md dark:bg-darker">
-                  <div>
-                    <h6
-                      class="text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light"
-                    >
-                      Publications
-                    </h6>
-                    <span class="text-xl font-semibold">12000</span>
-                    
-                  </div>
-                  <div>
-                    <span>
-                      <svg
-                        class="w-12 h-12 text-gray-300 dark:text-primary-dark"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
-                        />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-
-                <!--End  Value card -->
-
-                <!-- Users card -->
-                <div class="flex items-center justify-between p-4 bg-white rounded-md dark:bg-darker">
-                  <div>
-                    <h6
-                      class="text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light"
-                    >
-                      Users
-                    </h6>
-                    <span class="text-xl font-semibold">50,021</span>
-                    
-                  </div>
-                  <div>
-                    <span>
-                      <svg
-                        class="w-12 h-12 text-gray-300 dark:text-primary-dark"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                        />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-                <!--End  Users card -->
-              <!--End  State cards -->
+        <!-- Content -->
+        <div class="mt-2">
+          <!-- State cards -->
+          <div class="grid grid-cols-1 gap-8 p-4 lg:grid-cols-2 xl:grid-cols-4">
+            <!-- Value card -->
+            <div class="flex items-center justify-between p-4 bg-white rounded-md dark:bg-darker">
+              <div>
+                <h6
+                  class="text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light"
+                >
+                  Publications
+                </h6>
+                <span class="text-xl font-semibold">12000</span>
+                
               </div>
-             
+              <div>
+                <span>
+                  <svg
+                    class="w-12 h-12 text-gray-300 dark:text-primary-dark"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
+                    />
+                  </svg>
+                </span>
+              </div>
             </div>
-            <!--End  Content -->
-          </main>
+
+            <!--End  Value card -->
+
+            <!-- Users card -->
+            <div class="flex items-center justify-between p-4 bg-white rounded-md dark:bg-darker">
+              <div>
+                <h6
+                  class="text-xs font-medium leading-none tracking-wider text-gray-500 uppercase dark:text-primary-light"
+                >
+                  Users
+                </h6>
+                <span class="text-xl font-semibold">50,021</span>
+                
+              </div>
+              <div>
+                <span>
+                  <svg
+                    class="w-12 h-12 text-gray-300 dark:text-primary-dark"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+            <!--End  Users card -->
+          <!--End  State cards -->
+          </div>
+          
+        </div>
+        <!--End  Content -->
+        <div class="w-full my-12 ">
+                <p class="text-xl pb-3 flex items-center">
+                    <i class="fas fa-list mr-3"></i> Managers
+                </p>
+                <div class="overflow-auto bg-white rounded-md dark:bg-darker">
+                    <table class="min-w-full leading-normal m-b-8">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-5 py-3 border-b border-gray-200  text-left text-xs font-semibold text-white-600 uppercase tracking-wider">
+                                    User
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b border-gray-200  text-left text-xs font-semibold text-white-600 uppercase tracking-wider">
+                                    Rol
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b border-gray-200  text-left text-xs font-semibold text-white-600 uppercase tracking-wider">
+                                    Created at
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b border-gray-200  text-left text-xs font-semibold text-white-600 uppercase tracking-wider">
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class=" whitespace-no-wrap">
+                                                Vera Carpenter
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                                    <p class=" whitespace-no-wrap">Admin</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                                    <p class=" whitespace-no-wrap">
+                                        Jan 21, 2020
+                                    </p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                                    <span
+                                        class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                        <span class="relative">Activo</span>
+                                    </span>
+                                </td>
+                            </tr>
+                            
+                            <tr>
+                                <td class="px-5 py-5  text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="whitespace-no-wrap">
+                                                Alonzo Cox
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5  text-sm">
+                                    <p class="whitespace-no-wrap">Admin</p>
+                                </td>
+                                <td class="px-5 py-5  text-sm">
+                                    <p class="whitespace-no-wrap">Jan 18, 2020</p>
+                                </td>
+                                <td class="px-5 py-5  text-sm">
+                                    <span
+                                        class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                                        <span aria-hidden
+                                            class="absolute inset-0 bg-red-200 opacity-50 rounded-full"></span>
+                                        <span class="relative">Inactive</span>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+        </div>
+      </main>
           
           </div>
         </div>
