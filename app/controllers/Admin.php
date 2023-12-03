5c4dc52d204cac3eff76d3762169a4eff221ddc9
MedHachami <moha.hach.9@gmail.com>
2023-12-03 18:46:25 +0100

    Update Admin.php

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -3,7 +3,9 @@
 Class Admin extends Controller {
 
     public function __construct(){
-
+        if(!userIsLoggedIn()){
+            redirect('users/login');
+        }
     }
 
     public function index(){
