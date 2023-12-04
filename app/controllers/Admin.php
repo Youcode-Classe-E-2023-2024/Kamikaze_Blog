537a3501e8b2bef135b3ee20c7dc1a18f5ae547a
MedHachami <moha.hach.9@gmail.com>
2023-12-04 12:13:36 +0100

    Update Admin.php

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,14 +1,21 @@
 <?php
 
 Class Admin extends Controller {
-
+    private $userModel ;
+    private $publicationModel ;
     public function __construct(){
-        if(!userIsLoggedIn()){
+        $this->userModel = $this->model('User');
+        $this->publicationModel = $this->model('Publication');
+        if(!adminIsLoggedIn()){
             redirect('users/login');
         }
     }
 
     public function index(){
-        return $this->view('admin/index');
+        $data =[
+            'numOfUsers' => $this->userModel->countUsers(),
+            'numOfPublications' => $this->publicationModel->countPublications(),
+        ];
+        return $this->view('admin/index' , $data);
     }
 }
