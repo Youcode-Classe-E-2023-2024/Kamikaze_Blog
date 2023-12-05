22224c15f98da48acabf8b4ff89aaa6309a12939
MedHachami <moha.hach.9@gmail.com>
2023-12-05 15:26:31 +0100

    Update moderator.php

--- app/views/admin/moderator.php
+++ app/views/admin/moderator.php
@@ -42,7 +42,7 @@
                                     <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="name" name="city" type="text"  placeholder="Enter the city" aria-label="Name">
                                 </div>
                                 <div> 
-                                    <?php if(!empty($data['img_err']))echo  '*' . $data['img_err']; ?></span>
+                                    <span class="text-red-700"><?php if(!empty($data['img_err']))echo  '*' . $data['img_err']; ?></span>
                                     <label class="block text-sm text-gray-600" for="file_input">Upload file</label>
                                     <input name="moderator_Img" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
                                 </div>
