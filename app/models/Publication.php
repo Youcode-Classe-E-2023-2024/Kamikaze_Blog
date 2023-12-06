7e63a5501c095c5bcc3b0db7ba6d6c0831273687
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-07 00:25:26 +0100

    Update Publication.php

--- app/models/Publication.php
+++ app/models/Publication.php
@@ -32,7 +32,7 @@ Class Publication {
 
     public function getCategories() {
         // Query
-        $this->db->query("SELECT DISTINCT name FROM category");
+        $this->db->query("SELECT DISTINCT * FROM category");
     
         if($this->db->execute()){
             $categories = $this->db->resultSet();
@@ -42,4 +42,56 @@ Class Publication {
 
         return $categories;
     }
+
+    public function allPublications() {
+        $this->db->query("SELECT * FROM publication ");
+        if($this->db->execute()){
+            $allPublications = $this->db->resultSet();
+        }
+        else{
+            die("error");
+        }
+        return $allPublications;
+    }
+
+    public function filterCategory($category ) {
+        // die($category);
+        $this->db->query("SELECT * FROM publication WHERE category_Id = :category ");
+        $this->db->bind(':category', $category);
+        if($this->db->execute()){
+            $filterCategory = $this->db->resultSet();
+        }
+        else{
+            die("error");
+        }
+        return $filterCategory;
+    }
+
+    public function filterCity($city ) {
+        $this->db->query("SELECT * FROM publication WHERE city_Id = :city ");
+        $this->db->bind(':city', $city);
+        if($this->db->execute()){
+            $filterCity = $this->db->resultSet();
+        }
+        else{
+            die("error");
+        }
+        return $filterCity;
+    }
+
+    public function filterCategoryCity($category, $city) {
+        $this->db->query("SELECT * FROM publication WHERE city_Id = :city AND category_Id = :category");
+        $this->db->bind(':category', $category);
+        $this->db->bind(':city', $city);
+    
+        if ($this->db->execute()) {
+            $filterResults = $this->db->resultSet();
+        } else {
+            die("Erreur lors de l'exécution de la requête SQL");
+        }
+    
+        return $filterResults;
+    }
+    
+
 }
