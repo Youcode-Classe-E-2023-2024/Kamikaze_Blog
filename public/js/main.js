decf3c59b0d1bf986f5a7b81a295b3ae7149137a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-07 15:25:21 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,58 @@
 let publication;
+
 function displayData(data) {
   // Use map to create an array of table rows
-  const rows = data.map((pub) =>{
-    console.log(pub)
-    // console.log(pub.id);
-    return(
-` 
-        <a href="" class="hover:no-underline text-xl  lg:ml-40 md:ml-20 ml-10 w-full max-w-sm mx-4 rounded-md shadow-md   hover:shadow-xl transition-shadow duration-300 ease-in-out bg-white">
-        <div class="flex items-end justify-end h-56 w-80 bg-cover" style="background-image: url('../public/img/categories/${pub.imgUrl}')">
-
-        </div>
+  const rows = data.map((pub) => {
+    console.log(pub);
+    return (
+      `<a href="" class="hover:no-underline text-xl  lg:ml-40 md:ml-20 ml-10 w-full max-w-sm mx-4 rounded-md shadow-md   hover:shadow-xl transition-shadow duration-300 ease-in-out bg-white">
+        <div class="flex items-end justify-end h-56 w-80 bg-cover" style="background-image: url('../public/img/categories/${pub.imgUrl}')"></div>
         <div class="px-5 py-3">
-            <h3 class="text-gray-700 uppercase  ">${pub.title}</h3>
-            <span class="text-gray-500 mt-2  ">${pub.prix} DH</span>
+          <h3 class="text-gray-700 uppercase">${pub.title}</h3>
+          <span class="text-gray-500 mt-2">${pub.prix} DH</span>
         </div>
+      </a>`
+    );
+  });
 
-        </a>
-`     
-)})
   const dataContainer = document.getElementById('dataContainer');
-
   dataContainer.innerHTML = rows.join('');
 }
 
+// Fetch data when the page loads
+fetch('http://localhost/Kamikaze_Blog/Publications/filter/all/all')
+  .then(response => response.json())
+  .then(data => {
+    // Assign the data to the global variable
+    publication = data;
+
+    // Display the data initially
+    displayData(publication);
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('Error:', error);
+  });
+
 const save = document.getElementById('save');
-save.addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    var myForm = document.getElementById('myForm');
-    var selectedValue1 = myForm.elements['category'].value;
-    var selectedValue2 = myForm.elements['city'].value;
- 
-    fetch(`http://localhost/Kamikaze_Blog/Publications/filter/${selectedValue1}/${selectedValue2}`)
+save.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var myForm = document.getElementById('myForm');
+  var selectedValue1 = myForm.elements['category'].value;
+  var selectedValue2 = myForm.elements['city'].value;
+
+  fetch(`http://localhost/Kamikaze_Blog/Publications/filter/${selectedValue1}/${selectedValue2}`)
     .then(response => response.json())
     .then(data => {
       // Assign the data to the global variable
       publication = data;
 
+      // Display the updated data
       displayData(publication);
     })
-
     .catch(error => {
-        // Handle errors here
-        console.error('Error:', error);
+      // Handle errors here
+      console.error('Error:', error);
     });
-}
-
-
-
-)
-
+});
