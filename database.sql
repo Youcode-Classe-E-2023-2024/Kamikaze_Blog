f946e6af6b89678fe776de69de7c3fc8ed9f412d
MedHachami <moha.hach.9@gmail.com>
2023-12-09 17:25:51 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -80,6 +80,12 @@ CREATE TABLE likes (
     FOREIGN KEY (idPublication) REFERENCES publication(id),
     FOREIGN KEY (idUser) REFERENCES users(id)
 );
+CREATE TABLE team (
+                      id int PRIMARY KEY AUTO_INCREMENT,
+                      username varchar(255) NOT NULL,
+                      role varchar(255) NOT NULL,
+                      description varchar(255) NOT NULL
+) ;
 
 -- Insertion
 
