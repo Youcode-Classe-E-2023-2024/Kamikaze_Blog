2f808f8f5c70a4d947cfad75e21a9ccd8ff39525
MedHachami <moha.hach.9@gmail.com>
2023-12-10 01:09:19 +0100

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,31 +3,31 @@
     private $userModel;
     private $publicationModel;
     private $aboutModel;
-    
+
     public function __construct(){
-      
+
       $this->userModel = $this->model('User');
       $this->aboutModel = $this->model('about');
       $this->publicationModel = $this->model('Publication');
     }
-    
+
     public function index(){
-      
+
      $Publication = $this->publicationModel->homepub();
      $data=[
       'pub' => $Publication,
      ];
       $this->view('pages/index', $data);
-      
+
 
     }
     public function categories(){
       $cities = $this->allCities();
       $categories = $this->allCategories();
- 
+
       $data =[
-        'categories' => $categories, 
-        'cities'=>$cities, 
+        'categories' => $categories,
+        'cities'=>$cities,
       ];
       $this->view('pages/categories' , $data);
     }
@@ -37,7 +37,7 @@
 
     public function details($id){
 
- 
+
       $publication_result = $this->publicationModel->get_publicatin_byId($id);
 
       $category_Id = $publication_result->category_Id;
@@ -45,7 +45,7 @@
       $result_publication_category = $this->publicationModel->get_publication_category($category_Id , $id);
 
 
-      
+
       $data = [
         'publication' => $publication_result,
         'publication_category' => $result_publication_category,
@@ -54,33 +54,34 @@
       $this->view('pages/details', $data);
 
     }
-    
+
 
 
     public function allCities(){
      $cities = $this->publicationModel->getCities();
-     
+
       return $cities;
     }
     public function allCategories(){
        $categories = $this->publicationModel->getCategories();
- 
+
         return $categories;
     }
-    
+
 
 
 
     public function  contact(){
         $this->view('pages/contact');
     }
-    
-    
+
+
     public function add(){
         $categories = $this->allCategories();
+        $cities = $this->publicationModel->getCities();
         $data =[
            'category'=>$categories,
-
+            'cities' =>$cities,
         ];
       $this->view('users/addpost' , $data);
 
@@ -88,17 +89,17 @@
 
 
 
-      
-      
+
+
     public function about(){
 
       $about_data = $this->aboutModel->cmnt_about();
       $data = [
         'about'=> $about_data
       ];
-     
+
     return  $this->view('pages/aboutus',$data);
-    } 
+    }
 
 
   }
